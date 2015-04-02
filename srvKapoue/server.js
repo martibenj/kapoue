395fcac4ce8ff941d468028beb6db9377f4ba592
Benjamin Martinelli <benjamin.martinelli@atos.net>
2015-04-02 17:40:01 +0200

    Format server.js

--- srvKapoue/server.js
+++ srvKapoue/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var http = require('http');
 var busboy = require('connect-busboy');
 var fs = require('fs');
-var walk    = require('walk');
+var walk = require('walk');
 
 var app = express();
 
@@ -25,46 +25,37 @@ app.use(function(req, res, next) {
     next()
 });
 
-// utilisation du repertoire cliKapoue en static
+// Utilisation du repertoire cliKapoue en static
 app.use("/", express.static(__dirname + '/cliKapoue'));
-// binding des objets dans img avec l'url /images/
-app.use("/images", express.static(__dirname + '/img'));
 
-// Kapoue 1 with fake objects "name"
-app.get('/kapoue', function(req, res) {
-    res.contentType('application/json');
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE');
-    res.json(
-        [
-        {"name": "wine1"},
-        {"name": "wine2"}
-    ]);
-});
-console.log("Binded App /kapoue");
+// Storing img dir
+var imgDir = "/img";
+var absoluteImgDir = __dirname + imgDir;
+// Binding des objets dans img avec l'url /images/
+app.use("/images", express.static(absoluteImgDir));
 
-// Kapoue 2 with more attr
+// App /kapoue
 app.get('/kapoue/:id', function(req, res) {
     console.log("Calling with id=" + req.params.id);
     res.contentType('application/json');
     res.json(
     {
-      "petiteKapoue": {
-	"id": req.params.id,
-	"name": "Elle a vu la boite de mélange à purée",
-	"description": "Et direct ! Elle a fait \"Kapouuuuééééé\" !!"
-      }
-    });
+        "petiteKapoue": {
+            "id": req.params.id,
+            "name": "Elle a vu la boite de mélange à purée",
+            "description": "Et direct ! Elle a fait \"Kapouuuuééééé\" !!"
+        }
+    }
+    );
 });
-console.log("Binded App /photos");
+console.log("Binded App /kapoue/:id");
 
-// Image de Kapoue
+// Images de Kapoue
 app.get('/photos', function(req, res)
 {
-    // recherche des fichiers dans le repertoire img
+    // Recherche des fichiers dans le repertoire img
     var files   = [];
-    var walker  = walk.walk('./img', { followLinks: false });
+    var walker  = walk.walk('./'+imgDir, { followLinks: false });
     walker.on('file', function(root, stat, next) {
         // Add this file to the list of files
         files.push(stat.name);
@@ -72,7 +63,7 @@ app.get('/photos', function(req, res)
     });
 
     walker.on('end', function() {
-        // fin du parcours, on créea un objet json et on retourne la reponse
+        // fin du parcours, on créé un objet json et on retourne la reponse
         var jObject = [];
         var i;
         for(i in files)
@@ -89,20 +80,20 @@ app.get('/photos', function(req, res)
         res.json(jObject);
     });
 });
-console.log("Binded App /photo/:id");
+console.log("Binded App /photos");
 
 // upload de fichier
 app.post('/upload', function(req, res) {
-          console.log("appel post");
+    console.log("appel post");
     var fstream;
     req.pipe(req.busboy);
     req.busboy.on('file', function (fieldname, file, filename) {
         if (filename) {
             console.log("Upload du fichier: " + filename);
-            fstream = fs.createWriteStream(__dirname + '/img/' + filename);
+            fstream = fs.createWriteStream(absoluteImgDir + '/' + filename);
             file.pipe(fstream);
             fstream.on('close', function () {
-                res.redirect('/');
+                res.redirect('http://www.youtube.com/watch?v=oavMtUWDBTM');
             });
         }
     });
